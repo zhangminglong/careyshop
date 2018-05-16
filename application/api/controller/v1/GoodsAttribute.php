8751dd2469d31a00b246d11e49c2cc6a267734ab
思维空间 <dnyz520@126.com>
2018-05-16 23:30:29 +0800

    修复单词写错

--- application/api/controller/v1/GoodsAttribute.php
+++ application/api/controller/v1/GoodsAttribute.php
@@ -24,31 +24,31 @@ class GoodsAttribute extends CareyShop
     {
         return [
             // 添加一个商品属性主体
-            'add.goods.attrbute.body.item' => ['addAttributeBodyItem'],
+            'add.goods.attribute.body.item' => ['addAttributeBodyItem'],
             // 编辑一个商品属性主体
-            'set.goods.attrbute.body.item' => ['setAttributeBodyItem'],
+            'set.goods.attribute.body.item' => ['setAttributeBodyItem'],
             // 获取一个商品属性主体
-            'get.goods.attrbute.body.item' => ['getAttributeBodyItem'],
+            'get.goods.attribute.body.item' => ['getAttributeBodyItem'],
             // 获取商品属性主体列表
-            'get.goods.attrbute.body.list' => ['getAttributeBodyList'],
+            'get.goods.attribute.body.list' => ['getAttributeBodyList'],
             // 设置商品属性主体排序
-            'set.goods.attrbute.body.sort' => ['setAttributeSort'],
+            'set.goods.attribute.body.sort' => ['setAttributeSort'],
             // 添加一个商品属性
-            'add.goods.attrbute.item'      => ['addAttributeItem'],
+            'add.goods.attribute.item'      => ['addAttributeItem'],
             // 编辑一个商品属性
-            'set.goods.attrbute.item'      => ['setAttributeItem'],
+            'set.goods.attribute.item'      => ['setAttributeItem'],
             // 批量删除商品属性
-            'del.goods.attrbute.list'      => ['delAttributeList'],
+            'del.goods.attribute.list'      => ['delAttributeList'],
             // 获取一个商品属性
-            'get.goods.attrbute.item'      => ['getAttributeItem'],
+            'get.goods.attribute.item'      => ['getAttributeItem'],
             // 获取商品属性列表
-            'get.goods.attrbute.list'      => ['getAttributeList'],
+            'get.goods.attribute.list'      => ['getAttributeList'],
             // 批量设置商品属性检索
-            'set.goods.attrbute.index'     => ['setAttributeIndex'],
+            'set.goods.attribute.index'     => ['setAttributeIndex'],
             // 批量设置商品属性是否核心
-            'set.goods.attrbute.important' => ['setAttributeImportant'],
+            'set.goods.attribute.important' => ['setAttributeImportant'],
             // 设置商品属性排序
-            'set.goods.attrbute.sort'      => ['setAttributeSort'],
+            'set.goods.attribute.sort'      => ['setAttributeSort'],
         ];
     }
 }
