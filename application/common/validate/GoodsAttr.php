d2198e6f87a5a35790eb85349eb308ab52f3095e
思维空间 <dnyz520@126.com>
2018-05-22 00:57:51 +0800

    验证规则修改

--- application/common/validate/GoodsAttr.php
+++ application/common/validate/GoodsAttr.php
@@ -22,6 +22,7 @@ class GoodsAttr extends CareyShop
         'goods_attribute_id' => 'require|integer|gt:0',
         'parent_id'          => 'require|integer|egt:0',
         'is_important'       => 'require|in:0,1',
+        'attr_value'         => 'max:150',
         'sort'               => 'integer|between:0,255',
     ];
 
@@ -34,6 +35,7 @@ class GoodsAttr extends CareyShop
         'goods_attribute_id' => '商品属性中的商品属性编号',
         'parent_id'          => '商品属性中的商品属性主体',
         'is_important'       => '商品属性中的是否核心属性',
+        'attr_value'         => '商品属性中的属性值',
         'sort'               => '商品属性中的排序值',
     ];
 }
