efc7ae5d54614e1047b37924735a23a1788fab3e
思维空间 <dnyz520@126.com>
2018-06-07 16:04:29 +0800

    增强标识判断

--- application/common/model/AppInstall.php
+++ application/common/model/AppInstall.php
@@ -243,6 +243,10 @@ class AppInstall extends CareyShop
                 ->where(['user_agent' => ['eq', $data['user_agent']]]);
         });
 
+        if ($result->isEmpty()) {
+            return $this->setError('不存在标识为 ' . $data['user_agent'] . ' 的应用安装包');
+        }
+
         foreach ($result as $value) {
             if (version_compare($value->getAttr('ver'), $data['ver'], '>')) {
                 return true;
