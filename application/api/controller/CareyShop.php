5258aba0cee5237b97b0c1d02da7335536be60a8
思维空间 <dnyz520@126.com>
2018-06-17 20:14:03 +0800

    为客户端API编写做准备

--- application/api/controller/CareyShop.php
+++ application/api/controller/CareyShop.php
@@ -361,8 +361,8 @@ class CareyShop extends Controller
     {
         // 初始账号数据
         $GLOBALS['client'] = [
-            'type'        => config('ClientGroup.' . ($this->apiDebug ? 'admin' : 'visitor'))['value'],
-            'group_id'    => $this->apiDebug ? AUTH_SUPER_ADMINISTRATOR : AUTH_GUEST,
+            'type'        => config('ClientGroup.' . ($this->apiDebug ? 'user' : 'visitor'))['value'],
+            'group_id'    => $this->apiDebug ? AUTH_CLIENT : AUTH_GUEST,
             'client_id'   => $this->apiDebug ? 1 : 0,
             'client_name' => $this->apiDebug ? 'CareyShop' : '游客',
         ];
--- application/extra/CareyShop.php
+++ application/extra/CareyShop.php
@@ -12,12 +12,12 @@
 return [
     // 产品信息
     'product'      => [
-        'product_name'    => 'CareyShop商城框架系统',
+        'product_name'    => 'CareyShop商城框架系统(标准版)',
         'product_version' => '1.1.0',
         'build_version'   => '201704211135',
         'product_website' => 'http://www.careyshop.cn',
         'product_update'  => 'http://www.careyshop.cn/checkUpdate',
-        'develop_team'    => 'CareyShop',
+        'develop_team'    => 'think',
         'company_name'    => '宁波楷瑞网络工作室',
         'company_website' => 'http://www.careyshop.cn',
     ],
