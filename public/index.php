0a090420b318b8a6c96a7e0a900ba053773c2197
思维空间 <dnyz520@126.com>
2018-07-02 09:48:24 +0800

    去除版本号

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  * CareyShop    应用入口文件
  *
  * @author      zxm <252404501@qq.com>
- * @version     v1.1
+
  * @date        2017/4/24
  */
 
--- public/router.php
+++ public/router.php
@@ -5,7 +5,7 @@
  * CareyShop    PHP自带WebServer支持
  *
  * @author      zxm <252404501@qq.com>
- * @version     v1.1
+ 
  * @date        2017/4/24
  */
 
